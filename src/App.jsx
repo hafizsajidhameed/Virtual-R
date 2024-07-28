3fda88f40446089af185f66888437876b2c3eafb
Sajid Hameed <hafizsajidhameed@gmail.com>
2024-07-28 21:03:58 +0200

    from office

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { FeatureSection } from "./components/FeatureSection";
 import NavBar from "./components/navbar";
+import  {HeroSection}  from "./components/HeroSection";
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <FeatureSection />
-    </>
+   <>
+   <NavBar />
+   <div className="max-w-7xl mx-auto pt-20 px-6">
+    <HeroSection />
+    <FeatureSection />
+   </div>
+   </>
   );
 }
 
